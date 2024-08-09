ba4d8a3497e3b94ef99d9b0a28d197993b97a2c5
k-rea <kitahara@k-rea.com>
2024-08-09 15:45:03 +0900

    Or型

--- src/main.rs
+++ src/main.rs
@@ -29,6 +29,34 @@ struct OrderLine {
     price: f64,
 }
 
+enum ProductCode {
+    Widget(WidgetCode),
+    Gizmo(GizmoCode),
+}
+
+struct WidgetCode(String);
+struct GizmoCode(String);
+
+enum OrderQuantity {
+    Unit(UnitQuantity),
+    Kilogram(KilogramQuantity)
+}
+struct UnitQuantity(i32);
+struct KilogramQuantity(f64);
+
+
+struct PersonalName {
+    first_name: String50,
+    middle_initial: Option<String1>,
+    last_name: String50,
+}
+
+struct String50(String);
+struct String1(char);
+
+struct EmailAddress(String);
+
+
 fn main() {
     println!("Hello, functional domain modeling!");
 }
