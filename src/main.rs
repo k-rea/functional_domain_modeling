6b6bf5888b8bea2a4ab9af3901d3a41d0952b3ad
k-rea <kitahara@k-rea.com>
2024-08-09 15:22:00 +0900

    4.3.1 AND型

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,25 @@ struct PricedOrder;
 type ValidateOrder = fn(UnvalidatedOrder) -> ValidatedOrder;
 type PriceOrder = fn(ValidatedOrder) -> PricedOrder;
 
+struct Order {
+    id: OrderId,
+    customer_info: CustomerInfo,
+    shipping_address: ShippingAddress,
+    billing_address: BillingAddress,
+}
+
+struct CustomerInfo {
+    id: CustomerId,
+    name: String,
+    email: String,
+}
+
+struct OrderLine {
+    product_code: String,
+    quantity: i32,
+    price: f64,
+}
+
 fn main() {
     println!("Hello, functional domain modeling!");
 }
